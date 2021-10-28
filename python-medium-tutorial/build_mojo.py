b59fb555864ba6f05a0f91ea741fb63b12a01c94
Stefano Mariani, PhD <stefano.mariani@unimore.it>
2021-10-28 15:01:15 +0200

    now saving also model performance and input param

--- python-medium-tutorial/build_mojo.py
+++ python-medium-tutorial/build_mojo.py
@@ -17,8 +17,16 @@ MODEL_KEY = "model"
 FOLDS_KEY = "nfolds"
 MOJO_KEY = "mojo_path"
 
-GBE_KEY = "GBE"
+GBE_SEL = "GBE"
+PARAMS_OUT = "_trainparams"
+PERFORMANCE = "_performance"
 ##### <===== CONFIGURATION
+
+MRD_KEY = "mean_residual_deviance"
+RMSLE_KEY = "RMSLE"
+MAE_KEY = "MAE"
+RMSE_KEY = "RMSE"
+MSE_KEY = "MSE"
 
 # load configuration
 with open(CONFIG_FILENAME) as infile:
@@ -37,14 +45,14 @@ train_cols = [x for x in data.col_names if x != target]
 train, test = data.split_frame(ratios=[config[SPLIT_KEY], ])
 
 # model selection
-if config[MODEL_KEY] == GBE_KEY:
+if config[MODEL_KEY] == GBE_SEL:
     model = H2OGradientBoostingEstimator(nfolds=config[FOLDS_KEY], **params)
 else:  # add more models here, provided they are imported
     sys.exit(f"ERROR: Model {config[MODEL_KEY]} not supported!")
 
 # TRAIN
 model.train(x=train_cols, y=target, training_frame=train)
-print(model.model_performance(test))  # TODO this could be also print out to a file and later shown by frontend
+#print(model.model_performance(test))  # TODO this could be also print out to a file and later shown by frontend
 
 # write mojo to disk with meaningful name
 timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
@@ -58,3 +66,28 @@ filepath = model.download_mojo(path=mojopath, genmodel_name=f"{mojoname}.jar", g
 # rename zip as option below only controls jar name
 filename = Path(filepath).stem
 os.rename(f"{filepath}", f"{mojopath}/{mojoname}.zip")
+
+# also write out training params
+with open(f'{mojopath}/{mojoname}{PARAMS_OUT}.json', 'w') as params_json:
+    json.dump(params, params_json, indent=2)
+
+# also write out performance params
+perf_dict = {}
+perf_dict["learnt_params"] = {}
+perf_dict["performance"] = {}
+perf_dict["features_importance"] = {}
+if model.type == "regressor":
+    metrics = model.model_performance(test)
+    perf_dict["performance"][MSE_KEY] = metrics.mse()
+    perf_dict["performance"][RMSE_KEY] = metrics.rmse()
+    perf_dict["performance"][MAE_KEY] = metrics.mae()
+    perf_dict["performance"][RMSLE_KEY] = metrics.rmsle()
+    perf_dict["performance"][MRD_KEY] = metrics.mean_residual_deviance()
+    for f in model.varimp():
+        perf_dict["features_importance"][f[0]] = {}
+        perf_dict["features_importance"][f[0]]['relative'] = f[1]
+        perf_dict["features_importance"][f[0]]['scaled'] = f[2]
+        perf_dict["features_importance"][f[0]]['percentage'] = f[3]
+
+with open(f'{mojopath}/{mojoname}{PERFORMANCE}.json', 'w') as params_json:
+    json.dump(perf_dict, params_json, indent=2)
